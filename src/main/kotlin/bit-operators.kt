27c211392aa4b41dbb89aac35874ca8eb2d1790d
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-18 07:47:58 -0400

    Bit Operators

---
+++ src/main/kotlin/bit-operators.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val x = 10;
+    val y = 7;
+
+    println(x or y)
+    println(x and y)
+    println(x xor y)
+    print(x.inv())
+    println(x shl 1)
+    println(y shl 5)
+}
